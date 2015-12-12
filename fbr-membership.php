1dcd5715f3473dd1b06bf39ca7d89bce531616c8
Fahri Arrasyid <fahri.arrasyid@gmail.com>
2015-12-12 20:32:45 +0700

    Add membership menu

---
+++ fbr-membership.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: FBR Membership
+Plugin URI: http://fabrictheme.com
+Description: Wordpress Membership Plugin
+Version: 0.2.0
+Author: Fahri Arrasyid
+Author URI: http://github.com/mfahri
+
+/**
+  * @function 	: fbr_member_menu
+  *	@desc 		: This function will create menu in admin wordpress
+  */
+add_action("admin_menu", "fbr_member_menu");
+function fbr_member_menu() {
+	add_menu_page( 'Membership' , 'Membership', 'add_users', 'fbr-membership', 'fbr_member_init', '', 14.2 );
+    add_submenu_page("fbr-membership", "Add New", "Add New", 'add_users', "fbr-member-add", "fbr_member_add");
+    add_submenu_page("fbr-membership", "Settings", "Settings", 'add_users', "fbr-member-settings", "fbr_member_settings");
+}
+
+function fbr_member_init(){
+
+}
